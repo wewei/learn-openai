61622a5fa4556062e9364848ca595c08ed3594cd
Wei Wei <shazhouke@gmail.com>
2024-03-11 22:00:56 +0800

    support form input in console

--- chat-game/console-agent.ts
+++ chat-game/console-agent.ts
@@ -1,7 +1,12 @@
 import * as readline from "readline";
 import { Agent } from ".";
+import { JSONSchema7, validate } from "json-schema";
 
 export function consoleAgent(name: string, rl: readline.Interface): Agent {
+    function question(prompt: string): Promise<string> {
+        return new Promise((resolve) => rl.question(prompt, resolve));
+    }
+
     return {
         init: async () => {},
         send: async (messages) => {
@@ -14,19 +19,18 @@ export function consoleAgent(name: string, rl: readline.Interface): Agent {
         chat: async (instructions, audiences) => {
             rl.write(`[${name}] Instructions: ${instructions}\n`);
             rl.write(`[${name}] Audience: ${audiences.join(", ")}\n`);
-            return new Promise((resolve) => {
-                rl.question(`[${name}]: `, (content) => {
-                    resolve(content);
-                });
-            });
+            return question(`[${name}]: `);
         },
         form: async (instructions, form) => {
             rl.write(`[${name}] Instructions: ${instructions}\n`);
-            return new Promise((resolve) => {
-                rl.question(`[${name}]: `, (content) => {
-                    resolve(content);
-                });
-            });
+            const { properties = {} } = form.type;
+            const result: Record<string, any> = {};
+            for (const key in properties) {
+                do {
+                    result[key] = await question(`[${name}] <${key}>: `);
+                } while (!validate(result[key], properties[key] as JSONSchema7).valid);
+            }
+            return result;
         },
     };
 }

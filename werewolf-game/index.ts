107c777f33ead1acc5a472894a08438c82db70af
Scott Wei <leescott@microsoft.com>
2024-03-12 19:46:31 +0800

    Implement the game rules

--- werewolf-game/index.ts
+++ werewolf-game/index.ts
@@ -1,7 +1,6 @@
 import { resolve } from "path";
 import { readFile } from "fs/promises";
-import { ExtMessage, GameRule, GameRuleCallbacks } from "../chat-game";
-import { Form } from "../chat-game/form";
+import { GameRule, GameRuleCallbacks, play } from "../chat-game";
 
 export type Role =
     | "Villager"
@@ -11,11 +10,12 @@ export type Role =
     | "Guardian"
     | "Werewolf";
 export type GameState = {
-    killed: Record<string, boolean>;
+    livingPlayers: string[];
     roles: Record<string, Role>;
     lastGuard: string | null;
     potionUsed: boolean;
     poisonUsed: boolean;
+    round: number;
 };
 
 function loadText(fileName: string): Promise<string> {
@@ -47,11 +47,9 @@ function shuffleArray<T>(array: T[]): T[] {
 
 function livingPlayersWithRole(
     role: Role,
-    { roles, killed }: GameState
+    { roles, livingPlayers }: GameState
 ): string[] {
-    return Object.keys(roles).filter(
-        (name) => roles[name] === role && !killed[name]
-    );
+    return livingPlayers.filter((name) => roles[name] === role);
 }
 
 async function guardianRound(
@@ -84,7 +82,7 @@ async function guardianRound(
                         enum: livingPlayers.filter((p) => p !== lastGuard),
                     },
                 },
-                required: ['player'],
+                required: ["player"],
             }
         );
         await send([
@@ -171,11 +169,11 @@ async function witchRound(
         potionUsed,
         poisonUsed,
     }: {
-        livingWitch: string[],
-        killingPlayer: string,
-        livingPlayers: string[],
-        potionUsed: boolean,
-        poisonUsed: boolean,
+        livingWitch: string[];
+        killingPlayer: string;
+        livingPlayers: string[];
+        potionUsed: boolean;
+        poisonUsed: boolean;
     },
     { form, send }: GameRuleCallbacks
 ): Promise<{ healed: boolean; poisonedPlayer: string | null }> {
@@ -188,7 +186,7 @@ async function witchRound(
     if (!potionUsed && !poisonUsed) {
         const { healed, player: poisonedPlayer = null } = await form(
             witch,
-            `The player ${killingPlayer} is being killed, do you want to save him/her with your only potion? The living players or ${livingPlayers.join(
+            `The player ${killingPlayer} is being killed, do you want to save him/her with your only potion? The living players are ${livingPlayers.join(
                 ", "
             )}, do you want to eliminate any of them with your only poison? You need make your decisions to maximize the villagers and the special characters chances to win.`,
             {
@@ -197,8 +195,7 @@ async function witchRound(
                 properties: {
                     healed: {
                         type: "boolean",
-                        description:
-                            "Whether to heal the player with potion",
+                        description: "Whether to heal the player with potion",
                     },
                     player: {
                         type: "string",
@@ -238,8 +235,7 @@ async function witchRound(
                 properties: {
                     healed: {
                         type: "boolean",
-                        description:
-                            "Whether to heal the player with potion",
+                        description: "Whether to heal the player with potion",
                     },
                 },
                 required: ["heal"],
@@ -250,7 +246,9 @@ async function witchRound(
             {
                 audiences: livingWitch,
                 user: null,
-                content: healed ? `You saved player ${killingPlayer}.` : `You didn't save player ${killingPlayer}.`
+                content: healed
+                    ? `You saved player ${killingPlayer}.`
+                    : `You didn't save player ${killingPlayer}.`,
             },
         ]);
         return { healed, poisonedPlayer: null };
@@ -259,7 +257,7 @@ async function witchRound(
     if (!poisonUsed) {
         const { player: poisonedPlayer = null } = await form(
             witch,
-            `The living players or ${livingPlayers.join(
+            `The living players are ${livingPlayers.join(
                 ", "
             )}, do you want to eliminate any of them with your only poison? You need make your decisions to maximize the villagers and the special characters chances to win.`,
             {
@@ -280,13 +278,274 @@ async function witchRound(
             {
                 audiences: livingWitch,
                 user: null,
-                content: poisonedPlayer ? `and you poisoned ${poisonedPlayer}` : "and you didn't use the poison.",
+                content: poisonedPlayer
+                    ? `and you poisoned ${poisonedPlayer}`
+                    : "and you didn't use the poison.",
             },
         ]);
         return { healed: false, poisonedPlayer };
     }
 
     return { healed: false, poisonedPlayer: null };
+}
+
+function numberTh(n: number): string {
+    if (n === 1) return "first";
+    if (n === 2) return "second";
+    if (n === 3) return "third";
+    return `${n}th`;
+}
+
+async function seerRound(
+    {
+        livingSeer,
+        livingPlayers,
+        livingWerewolfs,
+    }: {
+        livingSeer: string[];
+        livingPlayers: string[];
+        livingWerewolfs: string[];
+    },
+    { form, send }: GameRuleCallbacks
+): Promise<void> {
+    const seer = livingSeer[0];
+
+    if (!seer) {
+        return;
+    }
+
+    const verifiablePlayers = livingPlayers.filter((p) => p !== seer);
+
+    const { player: testedPlayer = null } = await form(
+        seer,
+        `The living players besides you are ${verifiablePlayers.join(
+            ", "
+        )}, please select one to test if he/she is a werewolf.`,
+        {
+            name: "test",
+            description: "Test if a player is werewolf",
+            properties: {
+                player: {
+                    type: "string",
+                    description: "The player to test",
+                    enum: verifiablePlayers,
+                },
+            },
+            required: [],
+        }
+    );
+
+    const isWerewolf = livingWerewolfs.includes(testedPlayer);
+    send([
+        {
+            audiences: livingSeer,
+            user: null,
+            content: `You choose to test ${testedPlayer}, and he/she ${
+                isWerewolf ? "is" : "is not"
+            } a wereworf.`,
+        },
+    ]);
+
+    console.log(
+        `${seer} tested ${testedPlayer}, and he/she ${
+            isWerewolf ? "is" : "is not"
+        } a werewolf.`
+    );
+}
+
+async function announceRound({
+    livingPlayers,
+    shuffledEliminatedPlayers,
+    roles,
+}: {
+    livingPlayers: string[],
+    shuffledEliminatedPlayers: string[],
+    roles: Record<string, Role>,
+}, callbacks: GameRuleCallbacks): Promise<string[]> {
+    const { send } = callbacks;
+
+    send([
+        {
+            user: null,
+            audiences: livingPlayers,
+            content:
+                shuffledEliminatedPlayers.length > 1
+                    ? `Players ${shuffledEliminatedPlayers.join(
+                          ", "
+                      )} are eliminated.`
+                    : shuffledEliminatedPlayers.length > 0
+                    ? `Player ${shuffledEliminatedPlayers[0]} is eliminated.`
+                    : "No one was eliminated.",
+        },
+    ]);
+
+    let newLivingPlayers = livingPlayers.filter(p => !shuffledEliminatedPlayers.includes(p));
+
+    for (const player of shuffledEliminatedPlayers) {
+        newLivingPlayers = await eliminationRound({ player, livingPlayers: newLivingPlayers, roles }, callbacks);
+    }
+
+    return newLivingPlayers;
+}
+
+async function discussRound({
+    shuffledEliminatedPlayers,
+    livingPlayers,
+}: {
+    shuffledEliminatedPlayers: string[],
+    livingPlayers: string[],
+}, { chat, send }: GameRuleCallbacks): Promise<void> {
+    for (const player of livingPlayers) {
+        if (shuffledEliminatedPlayers.includes(player)) {
+            continue;
+        }
+
+        const content = await chat(
+            player,
+            "Please share out your point of view on who we should suspect or who we should trust. You comments may impact the vote of elimination during the day, please comment carefully to maximum your camp's chance to win.",
+            livingPlayers
+        );
+
+        await send([{
+            user: player,
+            content,
+            audiences: livingPlayers,
+        }]);
+    }
+}
+
+async function voteRound(
+    {
+        livingPlayers,
+        roles,
+    }: { livingPlayers: string[]; roles: Record<string, Role> },
+    callback: GameRuleCallbacks
+): Promise<string[]> {
+    let targetPlayers = livingPlayers;
+    const { form, send, chat } = callback;
+
+    while (targetPlayers.length > 1) {
+        const votes = await Promise.all(
+            livingPlayers.map((player) =>
+                form(player, "", {
+                    name: "vote",
+                    description: `Vote the player you want to eliminate in this round. In order to maximize your camp's chance to win. The vote targets are ${targetPlayers.join(
+                        ", "
+                    )}`,
+                    properties: {
+                        player: {
+                            type: "string",
+                            description: "The player you vote to eliminate",
+                            enum: targetPlayers,
+                        },
+                    },
+                    required: ["player"],
+                }).then((value) => ({ player, target: value.player as string }))
+            )
+        );
+
+        const { max, scores } = votes.reduce(
+            ({ max, scores }, { target }) => {
+                scores[target] ??= 0;
+                scores[target] += 1;
+                return {
+                    max: max < scores[target] ? scores[target] : max,
+                    scores,
+                };
+            },
+            { max: 0, scores: {} as Record<string, number> }
+        );
+
+        targetPlayers = targetPlayers.filter((p) => scores[p] === max);
+
+        await send([
+            {
+                user: null,
+                audiences: livingPlayers,
+                content: `The vote results are\n${votes
+                    .map(
+                        ({ player, target }) =>
+                            `* ${player} vote to eliminate ${target}`
+                    )
+                    .join("\n")}`,
+            },
+            {
+                user: null,
+                audiences: livingPlayers,
+                content:
+                    targetPlayers.length === 1
+                        ? `Player ${targetPlayers[0]} is eliminated`
+                        : `Players ${targetPlayers.join(
+                              ", "
+                          )} get the maximum ${max} votes, let's vote again, only in them.`,
+            },
+        ]);
+    }
+
+    return await eliminationRound({ player: targetPlayers[0], roles, livingPlayers }, callback);
+}
+
+export async function eliminationRound({
+    player,
+    livingPlayers,
+    roles,
+}: {
+    player: string,
+    livingPlayers: string[],
+    roles: Record<string, Role>,
+}, callbacks: GameRuleCallbacks): Promise<string[]> {
+    const { chat, form, send } = callbacks;
+    const content = await chat(
+        player,
+        "You were eliminated, please leave your last comment",
+        livingPlayers
+    );
+    await send([{
+        user: player,
+        content,
+        audiences: livingPlayers,
+    }]);
+
+    if (roles[player] === "Hunter") {
+        const result = await form(
+            player,
+            `As the Hunter, you can optionally choose a player to eliminate with your gun, available targets are ${livingPlayers.join(
+                ", "
+            )}.`,
+            {
+                name: "shoot",
+                description: "Shoot a player to elimination.",
+                properties: {
+                    player: {
+                        type: "string",
+                        description: "The playerto eliminate.",
+                        enum: livingPlayers,
+                    },
+                },
+                required: [],
+            }
+        );
+        if (result.player) {
+            const shotPlayer: string = result.player;
+            await send([{
+                user: null,
+                audiences: livingPlayers,
+                content: `Player ${player} is a hunter, he/she choose to shoot ${result.player}.`,
+            }]);
+
+            return eliminationRound({
+                player: result.player as string,
+                livingPlayers: livingPlayers.filter(p => p !== shotPlayer),
+                roles,
+            }, callbacks);
+        }
+        await send([{
+            user: null,
+            audiences: livingPlayers,
+            content: `Player ${player} is a hunter, he/she choose not to shoot.`,
+        }]);
+    }
+    return livingPlayers;
 }
 
 export async function werewolfGame(): Promise<GameRule<GameState>> {
@@ -311,11 +570,12 @@ export async function werewolfGame(): Promise<GameRule<GameState>> {
     return {
         init: async (users, { send }) => {
             const state: GameState = {
-                killed: {},
+                livingPlayers: users,
                 roles: {},
                 lastGuard: null,
                 potionUsed: false,
                 poisonUsed: false,
+                round: 1,
             };
             if (users.length !== 12) {
                 console.log(users);
@@ -338,6 +598,7 @@ export async function werewolfGame(): Promise<GameRule<GameState>> {
                     state.roles[shuffled[i]] = "Villager";
                 }
             }
+
             await send([
                 { user: null, audiences: users, content: strGameRules },
                 {
@@ -436,25 +697,36 @@ export async function werewolfGame(): Promise<GameRule<GameState>> {
             }
 
             // Night
+            await send([
+                {
+                    user: null,
+                    content: `Now is the ${numberTh(state.round)} night.`,
+                    audiences: livingPlayers,
+                },
+            ]);
+
             // 1. Guardian round
-            const guardPlayer = await guardianRound(
+            const guardedPlayer = await guardianRound(
                 { livingGuardian, livingPlayers, lastGuard: state.lastGuard },
                 callbacks
             );
-            console.log(`${guardPlayer} is guarded by ${livingGuardian.join(', ')}`);
+            console.log(
+                `${guardedPlayer} is guarded by ${livingGuardian.join(", ")}`
+            );
 
             // 2. Werewolf round
             const killingPlayer = await werewolfRound(
                 { livingPlayers, livingWerewolfs },
                 callbacks
             );
-            console.log(`${killingPlayer} is being killed by ${livingWerewolfs.join(', ')}`);
+            console.log(
+                `${killingPlayer} is being killed by ${livingWerewolfs.join(
+                    ", "
+                )}`
+            );
 
             // 3. Witch round
-            const {
-                healed,
-                poisonedPlayer,
-            } = await witchRound(
+            const { healed, poisonedPlayer } = await witchRound(
                 {
                     livingWitch,
                     killingPlayer,
@@ -465,8 +737,66 @@ export async function werewolfGame(): Promise<GameRule<GameState>> {
                 callbacks
             );
 
-            console.log(`${livingWitch} choose to ${healed ? "heal" : "not heal"} ${killingPlayer}, and use poison on ${poisonedPlayer}`);
-            return null;
+            console.log(
+                `${livingWitch} choose to ${
+                    healed ? "heal" : "not heal"
+                } ${killingPlayer}, and use poison on ${poisonedPlayer}`
+            );
+
+            // 4. Seer round
+            await seerRound(
+                { livingSeer, livingPlayers, livingWerewolfs },
+                callbacks
+            );
+
+            // Day
+            await send([
+                {
+                    user: null,
+                    content: `Now is the ${numberTh(state.round)} day.`,
+                    audiences: livingPlayers,
+                },
+            ]);
+
+            const eliminatedPlayers = [];
+            if (poisonedPlayer) {
+                eliminatedPlayers.push(poisonedPlayer);
+            }
+            if (killingPlayer && !healed && guardedPlayer !== killingPlayer) {
+                eliminatedPlayers.push(killingPlayer);
+            }
+            const shuffledEliminatedPlayers = shuffleArray(eliminatedPlayers);
+
+            // 1. Announce the night phase result
+            const livingPlayers1 = await announceRound(
+                {
+                    shuffledEliminatedPlayers,
+                    livingPlayers,
+                    roles: state.roles,
+                },
+                callbacks
+            );
+
+            // 2. Discuss round
+            await discussRound({
+                livingPlayers: livingPlayers1,
+                shuffledEliminatedPlayers,
+            }, callbacks);
+
+            // 3. Vote round
+            const livingPlayers2 = await voteRound({
+                livingPlayers: livingPlayers1,
+                roles: state.roles,
+            }, callbacks);
+
+            return {
+                roles: state.roles,
+                livingPlayers: livingPlayers2,
+                lastGuard: guardedPlayer,
+                poisonUsed: state.poisonUsed || poisonedPlayer !== null,
+                potionUsed: state.potionUsed || healed,
+                round: state.round + 1,
+            };
         },
     };
 }
